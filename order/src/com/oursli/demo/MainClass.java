40fd92dd62842475f1fe9dba389b0502043083f2
ziwen <admin@san0.cn>
2020-03-11 21:44:52 +0800

    create order module

---
+++ order/src/com/oursli/demo/MainClass.java
@@ -0,0 +1,15 @@
+package com.oursli.demo;
+
+/**
+ * Package:      com.oursli.demo
+ * ClassName:    MainClass
+ * Description:  订餐菜单系统 - mini
+ * Datetime:     2020/3/11   9:41 下午
+ * Author:       子文i
+ */
+public class MainClass {
+    public static void main(String[] args)
+    {
+
+    }
+}
